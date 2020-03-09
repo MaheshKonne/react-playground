4f45dc4238648ad5c214b1d9242327188ec8cef8
Mahesh Konne <maheshkonneiiitb@gmail.com>
2020-03-09 12:47:18 +0530

    remove unnecessary code

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,28 +1,13 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
 
-import A from './A';
-import Img from './Img';
-import NavBar from './NavBar';
-import HeaderLink from './HeaderLink';
-import Banner from './banner.jpg';
-import messages from './messages';
+import H2 from '../H2';
+import CenteredSection from '../CenteredSection';
 
 function Header() {
   return (
-    <div>
-      <A href="https://www.reactboilerplate.com/">
-        <Img src={Banner} alt="react-boilerplate - Logo" />
-      </A>
-      <NavBar>
-        <HeaderLink to="/">
-          <FormattedMessage {...messages.home} />
-        </HeaderLink>
-        <HeaderLink to="/features">
-          <FormattedMessage {...messages.features} />
-        </HeaderLink>
-      </NavBar>
-    </div>
+    <CenteredSection>
+      <H2> Welcome to react-playground!</H2>
+    </CenteredSection>
   );
 }
 
